b44c53a1429486a7492ea8636a51b8bd8fc2f28d
riders <rsadeqm@gmail.com>
2021-01-17 16:28:20 +0100

    Débugg of the game + scorring script révision

---
+++ Assets/Scripts/Mur2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mur2 : MonoBehaviour
+{
+    private int Score = 0;
+    public GameObject Projectil;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Score += 1;
+        GameObject.Find("ScoreG").GetComponent<TextMesh>().text = "" + Score;
+        Instantiate(Projectil, GameObject.Find("Eject").GetComponent<Transform>().transform.position, Quaternion.identity);
+        Destroy(collision.gameObject);
+    }
+}
